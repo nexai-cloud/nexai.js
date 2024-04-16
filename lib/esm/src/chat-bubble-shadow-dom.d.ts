c1acc7a02f9d4a47e1b7116968cfd3a1bc3cdb2b
Gabe L <gabe@fijiwebdesign.com>
2024-04-16 13:54:24 +0300

    build 1-8

--- lib/esm/src/chat-bubble-shadow-dom.d.ts
+++ lib/esm/src/chat-bubble-shadow-dom.d.ts
@@ -1,3 +1,2 @@
 import { NexaiChatBubbleProps } from './chat-bubble';
-import "construct-style-sheets-polyfill";
 export declare const NexaiChatBubbleShadowDom: (props: NexaiChatBubbleProps) => import("react/jsx-runtime").JSX.Element;
