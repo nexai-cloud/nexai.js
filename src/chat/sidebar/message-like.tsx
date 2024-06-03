ea1e69505d43cc170906e90a6a04d55a2555c08d
Gabe L <gabe@fijiwebdesign.com>
2024-06-03 15:02:07 +0300

    Fix button>button nesting. Fix clipRule, fillRule

--- src/chat/sidebar/message-like.tsx
+++ src/chat/sidebar/message-like.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 const ThumbUpFilled = ({ className }: { className?: string }) => (
   <svg className={className} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-    <path fill-rule="evenodd" d="M15.03 9.684h3.965c.322 0 .64.08.925.232.286.153.532.374.717.645a2.109 2.109 0 0 1 .242 1.883l-2.36 7.201c-.288.814-.48 1.355-1.884 1.355-2.072 0-4.276-.677-6.157-1.256-.472-.145-.924-.284-1.348-.404h-.115V9.478a25.485 25.485 0 0 0 4.238-5.514 1.8 1.8 0 0 1 .901-.83 1.74 1.74 0 0 1 1.21-.048c.396.13.736.397.96.757.225.36.32.788.269 1.211l-1.562 4.63ZM4.177 10H7v8a2 2 0 1 1-4 0v-6.823C3 10.527 3.527 10 4.176 10Z" clip-rule="evenodd"/>
+    <path fillRule="evenodd" d="M15.03 9.684h3.965c.322 0 .64.08.925.232.286.153.532.374.717.645a2.109 2.109 0 0 1 .242 1.883l-2.36 7.201c-.288.814-.48 1.355-1.884 1.355-2.072 0-4.276-.677-6.157-1.256-.472-.145-.924-.284-1.348-.404h-.115V9.478a25.485 25.485 0 0 0 4.238-5.514 1.8 1.8 0 0 1 .901-.83 1.74 1.74 0 0 1 1.21-.048c.396.13.736.397.96.757.225.36.32.788.269 1.211l-1.562 4.63ZM4.177 10H7v8a2 2 0 1 1-4 0v-6.823C3 10.527 3.527 10 4.176 10Z" clipRule="evenodd"/>
   </svg>
 )
 
@@ -23,7 +23,7 @@ const ThumbUpOutline = ({ className }: { className?: string }) => (
 
 const ThumbDownFilled = ({ className }: { className?: string }) => (
   <svg className={className} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-    <path fill-rule="evenodd" d="M8.97 14.316H5.004c-.322 0-.64-.08-.925-.232a2.022 2.022 0 0 1-.717-.645 2.108 2.108 0 0 1-.242-1.883l2.36-7.201C5.769 3.54 5.96 3 7.365 3c2.072 0 4.276.678 6.156 1.256.473.145.925.284 1.35.404h.114v9.862a25.485 25.485 0 0 0-4.238 5.514c-.197.376-.516.67-.901.83a1.74 1.74 0 0 1-1.21.048 1.79 1.79 0 0 1-.96-.757 1.867 1.867 0 0 1-.269-1.211l1.562-4.63ZM19.822 14H17V6a2 2 0 1 1 4 0v6.823c0 .65-.527 1.177-1.177 1.177Z" clip-rule="evenodd"/>
+    <path fillRule="evenodd" d="M8.97 14.316H5.004c-.322 0-.64-.08-.925-.232a2.022 2.022 0 0 1-.717-.645 2.108 2.108 0 0 1-.242-1.883l2.36-7.201C5.769 3.54 5.96 3 7.365 3c2.072 0 4.276.678 6.156 1.256.473.145.925.284 1.35.404h.114v9.862a25.485 25.485 0 0 0-4.238 5.514c-.197.376-.516.67-.901.83a1.74 1.74 0 0 1-1.21.048 1.79 1.79 0 0 1-.96-.757 1.867 1.867 0 0 1-.269-1.211l1.562-4.63ZM19.822 14H17V6a2 2 0 1 1 4 0v6.823c0 .65-.527 1.177-1.177 1.177Z" clipRule="evenodd"/>
   </svg>
 
 )
@@ -54,18 +54,18 @@ export const MessageLike = observer(({ chatMessage, className }: Props) => {
       className
     )}>
       <TooltipWrap className="bg-white" tooltip={'Like'}>
-        <button className="bg-muted bg-red" onClick={() => onThumb(1)}>
+        <div onClick={() => onThumb(1)}>
           {
             liked > 0 ? <ThumbUpFilled /> : <ThumbUpOutline />
           }
-        </button>
+        </div>
       </TooltipWrap>
       <TooltipWrap className="bg-white" tooltip={'Unlike'}>
-        <button onClick={() => onThumb(-1)}>
+        <div onClick={() => onThumb(-1)}>
           {
             liked < 0 ? <ThumbDownFilled /> : <ThumbDownOutline />
           }
-        </button>
+        </div>
       </TooltipWrap>
       
     </div>
